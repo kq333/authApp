801665cf82d87d3f84d9166f2a06948d4288bf66
kq333 <afugowski@yandex.com>
2023-09-24 09:58:41 +0200

    created component

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import './footer.scss';
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='footer__wrapper'>
+        <div>
+          created by <span className='footer__creator'>kq333</span>
+        </div>
+        <div>devChallenges.io</div>
+      </div>
+    </div>
+  );
+};
