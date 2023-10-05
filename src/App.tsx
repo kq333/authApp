bd7786a4aa69a347f3f3b28a7cd2c4680d15fa1f
kq333 <afugowski@yandex.com>
2023-10-05 22:11:40 +0200

    remove footer, added class to div elem

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import './App.scss';
+import { useState, useEffect } from 'react';
 
-import { FormComponent } from './components/form/formComponent';
+import { useSelector } from 'react-redux';
+import { EditSavedUserData } from './components/edit-saved';
+import { LoginRegisterComponent } from './components/login-register-component';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const isUserLogin = useSelector((state) => state.FireBaseReducer.isLogin);
+
+  useEffect(() => {
+    setIsLogin(isLogin);
+  }, [isLogin]);
+
   return (
     <div className='page'>
-
-
       <div className='page__wrapper'>
-      <main>
+        <main className='page__main'>
+          <section>{isUserLogin && <EditSavedUserData />}</section>
 
-        <section className='page__section'>
-          <FormComponent />
-        </section>
-      </main>
+          <section className='page__section'>
+            {!isUserLogin && <LoginRegisterComponent />}
+          </section>
+        </main>
+      </div>
     </div>
-    </div>
-
   );
 }
 
