e336326ce5bb5d18a30197164e658db93e61387e
kq333 <afugowski@yandex.com>
2023-10-06 00:54:50 +0200

    added fetchData function to get img from firebase

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,29 +1,103 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+import { useDispatch } from 'react-redux';
+import { logoutUser, setLogOut } from '../../features/authSlice';
+import { auth } from '../../../utils/fireBase';
 import { navbarIcons } from '../../../utils/helpers';
-import '../navbar/navbar.scss';
+import { useSelector } from 'react-redux';
 
+import { onAuthStateChanged } from 'firebase/auth';
+
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase, ref, get } from 'firebase/database';
+
+import '../navbar/navbar.scss';
 import arrowDown from '../../assets/icons/navbar/arrowDown.svg';
 import arrowUp from '../../assets/icons/navbar/arrowUp.svg';
-import devIcon from '../../assets/icons/devchallenges.svg';
+import personIcon from '../../assets/icons/photo/person.svg';
 
 export const Navbar = () => {
   const [isActive, setIsActive] = useState<boolean>(false);
-  const [isProfileActive, setIsProfileActive] = useState<string>(navbarIcons[0].name);
+  const [isProfileActive, setIsProfileActive] = useState<string>(
+    navbarIcons[0].name,
+  );
+
+  const [userData, setUserData] = useState<string>('');
 
   const changeIcon = !isActive ? arrowDown : arrowUp;
+
+  const isEditDataChanged = useSelector(
+    (state) => state.FireBaseReducer.isEditSaved,
+  );
+
+  console.log(isEditDataChanged);
+
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      dispatch(logoutUser());
+      dispatch(setLogOut(false));
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const userData = {
+          email: user.email,
+          photo: '',
+        };
+        setUserData(userData);
+      } else {
+        setUserData(null);
+      }
+    });
+  }, [isEditDataChanged]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const database = getDatabase();
+      const databaseRef = ref(database, 'users/' + auth.currentUser.uid);
+      const snapshot = await get(databaseRef);
+
+      if (snapshot.exists()) {
+        const dataBase = snapshot.val();
+        const { photo } = dataBase;
+
+        setUserData((prevUserData) => {
+          return {
+            ...prevUserData,
+
+            photo: photo,
+          };
+        });
+      } else {
+        console.log('No data found for the user.');
+      }
+    };
+
+    fetchData();
+  }, [isEditDataChanged]);
 
   return (
     <nav className='navbar'>
       <div className='navbar__wrapper'>
-        <div className='navbar__logo'>
-          <img src={devIcon} alt='logo' />
-        </div>
         <div className='navbar__login-section'>
           <div className='navbar__login-section-wrapper'>
             <div className='navbar__login-group-one'>
-              <img className='navbar__user-img' src='' alt='user image' />
-              <span className='navbar__user-name'>tomek knapik </span>
+              <img
+                className='navbar__user-img'
+                src={userData ? userData.photo : personIcon}
+                alt='user image'
+              />
+
+              <span className='navbar__user-email'>
+                {userData ? userData.email : ''}{' '}
+              </span>
               <img
                 className='navbar__handler-popup'
                 src={changeIcon}
@@ -38,6 +112,9 @@ export const Navbar = () => {
                   <div
                     className='navbar__login-group-two-wrapper'
                     key={elem.id}
+                    onClick={
+                      elem.name === 'Logout' ? () => handleLogout() : null
+                    }
                   >
                     <div
                       className={
