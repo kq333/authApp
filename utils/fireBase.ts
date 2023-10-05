ee0bc58d7ec702b7fecaed9332ac0282cbcc95ae
kq333 <afugowski@yandex.com>
2023-10-05 21:54:58 +0200

    changed file extension

---
+++ utils/fireBase.ts
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC67P3C0Ygs0tZ5-1sfQemvuVFNg-OGWlM",
+  authDomain: "auth-app-1911c.firebaseapp.com",
+  projectId: "auth-app-1911c",
+  storageBucket: "auth-app-1911c.appspot.com",
+  messagingSenderId: "233953448674",
+  appId: "1:233953448674:web:8936030e9bb0b388a784bb",
+  measurementId: "G-5990VZKYF7"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
---
+++ utils/helpers.ts
@@ -0,0 +1,14 @@
+export const socialIcons = [
+  { id: 1, name: 'google', img: '../src/assets/icons/social/Google.svg' },
+  { id: 2, name: 'facebook', img: '../src/assets/icons/social/Facebook.svg' },
+  { id: 3, name: 'twitter', img: '../src/assets/icons/social/Twitter.svg' },
+  { id: 4, name: 'github', img: '../src/assets/icons/social/Gihub.svg' },
+];
+
+export const navbarIcons = [
+  { id: 1, name: 'My profile', src: '../src/assets/icons/navbar/user.svg' },
+  { id: 2, name: 'Groupe Chat', src: '../src/assets/icons/navbar/people.svg' },
+  { id: 3, name: 'Logout', src: '../src/assets/icons/navbar/logout.svg' },
+];
+
+
