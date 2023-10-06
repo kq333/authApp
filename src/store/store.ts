650b391ed7d3ab81af0e1044d7d92db6dfb9327f
kq333 <afugowski@yandex.com>
2023-10-06 16:01:37 +0200

    changed store name

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import fireBaseReducer from '../features/authSlice'
+import thunkMiddleware from 'redux-thunk';
+
 export const store = configureStore({
     reducer: {
-        FireBaseReducer: fireBaseReducer,
+        auth: fireBaseReducer,
     },
+    middleware: [thunkMiddleware],
 })
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
