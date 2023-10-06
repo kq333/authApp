6cd319280f63a2acd8ca1456210b17f0965ada98
kq333 <afugowski@yandex.com>
2023-10-06 18:22:48 +0200

    deleted unnecessary content

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,8 +1,5 @@
-
 export const navbarIcons = [
   { id: 1, name: 'My profile', src: '../src/assets/icons/navbar/user.svg' },
   { id: 2, name: 'Groupe Chat', src: '../src/assets/icons/navbar/people.svg' },
   { id: 3, name: 'Logout', src: '../src/assets/icons/navbar/logout.svg' },
 ];
-
-
