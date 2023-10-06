db178c02b37984fad291d5b04faca58a8ed74959
kq333 <afugowski@yandex.com>
2023-10-06 18:09:20 +0200

    deleted unnecessary content

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,9 +1,3 @@
-export const socialIcons = [
-  { id: 1, name: 'google', img: '../src/assets/icons/social/Google.svg' },
-  { id: 2, name: 'facebook', img: '../src/assets/icons/social/Facebook.svg' },
-  { id: 3, name: 'twitter', img: '../src/assets/icons/social/Twitter.svg' },
-  { id: 4, name: 'github', img: '../src/assets/icons/social/Gihub.svg' },
-];
 
 export const navbarIcons = [
   { id: 1, name: 'My profile', src: '../src/assets/icons/navbar/user.svg' },
