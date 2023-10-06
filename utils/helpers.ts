dce1cad9afdbeab51f0286972c03d7d893c4a231
kq333 <afugowski@yandex.com>
2023-10-06 18:38:49 +0200

    refacor data content

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,9 @@
+import userIcon from '../src/assets/icons/navbar/user.svg';
+import peopleIcon from '../src/assets/icons/navbar/people.svg';
+import logoutIcon from '../src/assets/icons/navbar/logout.svg';
+
 export const navbarIcons = [
-  { id: 1, name: 'My profile', src: '../src/assets/icons/navbar/user.svg' },
-  { id: 2, name: 'Groupe Chat', src: '../src/assets/icons/navbar/people.svg' },
-  { id: 3, name: 'Logout', src: '../src/assets/icons/navbar/logout.svg' },
+  { id: 1, name: 'My profile', src: userIcon},
+  { id: 2, name: 'Groupe Chat', src: peopleIcon },
+  { id: 3, name: 'Logout', src: logoutIcon},
 ];
