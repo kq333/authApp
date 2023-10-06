fe47b186d07c437d385b3b37b031fa73ff5e997d
kq333 <afugowski@yandex.com>
2023-10-07 00:14:55 +0200

    remove useState and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { EditSavedUserData } from './components/edit-saved';
 import { LoginRegisterComponent } from './components/login-register-component';
@@ -7,19 +7,14 @@ import { useAppSelector } from './store/hooks';
 import { RootState } from './store/store';
 
 function App() {
-  const [isLogins, setIsLogins] = useState(false);
   const { isLogin } = useAppSelector((state: RootState) => state.auth);
-
-  useEffect(() => {
-    setIsLogins(isLogins);
-  }, [isLogins]);
 
   return (
     <div className='page'>
       <div className='page__wrapper'>
         <main className='page__main'>
           <section className='page__section'>
-            { !isLogin ? <LoginRegisterComponent /> : <EditSavedUserData />}
+            {!isLogin ? <LoginRegisterComponent /> : <EditSavedUserData />}
           </section>
         </main>
       </div>
